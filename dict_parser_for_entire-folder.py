b90c595916a7f350f125c42a8150fec00c474b7b
Google Jr <andilembele020@gmail.com>
2022-10-04 07:54:19 -0500

    parsing works like a charm

--- dict_parser_for_entire-folder.py
+++ dict_parser_for_entire-folder.py
@@ -18,7 +18,8 @@ def dict_parser():
         for filename in os.listdir(sys.argv[1]):
             if filename.endswith(".csv"):
                 fpath = Path(filename).parent
-                # create a new folder named 'cleaned' in the same directory as the file to be parsed and save the parsed file there
+                # create a new folder named 'cleaned' in the same directory as the files to be parsed and save the parsed files there
+
                 new_dir = os.path.join(fpath, 'cleaned')
                 if not os.path.exists(new_dir):
                     os.makedirs(new_dir)
@@ -27,14 +28,17 @@ def dict_parser():
 
                 # fname = Path(filename).name.rsplit(".", 1)[
                 #     0] + "_new_generated_file.csv"
-                with open(filename, 'r', encoding='utf8') as f:
-                    csv_reader = csv.DictReader(f, delimiter=',')
-
+                # fname = Path(filename).parent / fname
+                # get the file path of the file to be parsed and open it
+                file_path = os.path.join(sys.argv[1], filename)
+                with open(file_path, 'r') as csv_file:
+                    csv_reader = csv.reader(csv_file)
+                    
                     with open(os.path.join(fpath, fname), 'w', encoding='utf8') as new_users_table:
                         # Loop through the csv_reader object and return only fields that contain contain personal identifiable information (PII) and set those fields as fieldnames
                         fieldnames = [field for field in csv_reader.fieldnames if field in [
                             'first_name', 'firstname', 'last_name', 'lastname', 'email', 'password', 'passwordhash', 'phone_number', 'address', 'city', 'state', 'zip_code', 'country', 'comment_author', 'comment_author_email', 'comment_author_url', 'comment_author_IP', 'user_id', 'user_pass', 'user_login', 'ID', 'user_nicename', 'user_url', 'user_email', 'meta_value', 'id', 'username','phonenumber', 'bankacct', 'currency', 'address2', 'address1', 'postcode', 'issuenumber', 'startdate', 'credit', 'email_preferences', 'host', 'hostname', 'ip', 'ipaddress', 'ip_address', 'ipaddr', 'userid', 'nameservers', 'amount', 'ordernum', 'invoiceid', 'paymentmethod', 'domain', 'firstpayment', 'dedicatedip', 'company', 'companyname', 'name', 'nickname', 'confirmed_ip']]
-                        
+
                         csv_writer = csv.DictWriter(
                             new_users_table, fieldnames=fieldnames, delimiter=","
                         )
